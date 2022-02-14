807b38439e8a468a3c800e001291b2f8a9471db3
Nadher Arroum <nadher.arroum@gmail.com>
2022-02-14 17:57:12 +0100

    ADD display text to test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 let portApp = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.send('<h1>Hello World!</h1><h2>CI/CD Test</h2>');
 });
 
 var server = app.listen(portApp, function () {
