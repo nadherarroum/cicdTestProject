773ca6d0a7dabed7e4f0ce0bf8881367b33d6b96
Nadher Arroum <nadher.arroum@gmail.com>
2022-02-14 17:40:55 +0100

    ADD Procfile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+let portApp = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World!</h1>');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(portApp, function () {
   var host = server.address().address;
   var port = server.address().port;
 
